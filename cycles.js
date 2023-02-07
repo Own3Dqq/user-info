52cb1c2737c5102539ae6781870ba4c75e7cb0a0
Own3Dqq <own3dqq@gmail.com>
2023-02-07 22:55:26 +0200

    add another function who decide task

--- cycles.js
+++ cycles.js
@@ -3,8 +3,8 @@ let multiplicationTable = document.createElement('div');
 App.insertAdjacentElement('beforeend', multiplicationTable);
 multiplicationTable.className = `multiplicationTable`;
 
-/* ShowNumber Task */
-/* a  = 10 - меньше число, b  = 20 - більше число */
+/*і */
+/* Вивести на сторінку в один рядок через кому числа від 10 до 20 */
 const showNumber = (a, b) => {
 	let numbersArray = document.createElement('div');
 	numbersArray.className = `showNumber`;
@@ -17,7 +17,8 @@ const showNumber = (a, b) => {
 
 showNumber(10, 20);
 
-/* SquadNumber */
+/*  */
+/* Вивести квадрати чисел від 10 до 20 */
 const squadNumber = (a, b) => {
 	let squadNumberShow = document.createElement('div');
 	squadNumberShow.className = `squadNumber`;
@@ -28,9 +29,9 @@ const squadNumber = (a, b) => {
 };
 
 squadNumber(10, 20);
-/*  */
 
-/* multiplication table 7 */
+/*  */
+/* Вивести таблицю множення на 7 */
 const tableNumber = (num) => {
 	let table = document.createElement('div');
 	table.classList.add('table');
@@ -50,8 +51,7 @@ tableNumber(8);
 tableNumber(9);
 
 /*  */
-/* Arithmetic Mean  */
-
+/* Знайти середнє арифметичне всіх цілих чисел від 1 до 500  */
 const arithmeticAllNumber = (a, b) => {
 	let sum = 0;
 	let acc = 0;
@@ -64,4 +64,42 @@ const arithmeticAllNumber = (a, b) => {
 	return sum / acc;
 };
 
-console.log(arithmeticAllNumber(1, 500));
+arithmeticAllNumber(1, 500);
+
+/*  */
+/* Знайти суму всіх цілих чисел від 1 до 15 */
+
+function calculateTotal(number) {
+	let sum = 0;
+	for (let i = 1; i <= number; i++) {
+		sum += i;
+	}
+	return sum;
+}
+console.log(calculateTotal(15));
+
+/*  */
+/* Вивести суму лише парних чисел в діапазоні від 30 до 80.	 */
+const sumAllDoubleNumber = (a, b) => {
+	let arr = [];
+	for (let i = 0; a < b; a++) {
+		a % 2 === 0 ? arr.push(a) : undefined;
+	}
+	console.log(arr);
+	return arr;
+};
+
+sumAllDoubleNumber(30, 80);
+
+/*  */
+/* Вивести всі числа в діапазоні від 100 до 200 кратні 3. */
+
+const searchNumber = (a, b) => {
+	let arr = [];
+	for (let i = 0; a <= b; a++) {
+		a % 3 === 0 ? arr.push(a) : undefined;
+	}
+
+	console.log(arr);
+};
+searchNumber(100, 200);
